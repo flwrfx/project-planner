25b2f1132e0baf582e4b93727f48016e5b7b7339
flwrfx <briceflowers@hotmail.com>
2021-04-19 18:47:27 -0500

    Initial commit

---
+++ src/main/java/squad/dog/projectplanner/Controllers/HomeController.java
@@ -0,0 +1,16 @@
+package squad.dog.projectplanner.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String index(Model model){
+        return "index";
+    }//index
+
+
+}//HomeController
---
+++ src/test/java/squad/dog/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package squad.dog.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
